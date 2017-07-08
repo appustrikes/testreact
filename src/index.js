97c3367282cc5b4971de784dc2ee87bf6f2ca25d
Deepak Acharya <deepak.acharya@DACHARYAMACOS1.attlocal.net>
2017-07-08 00:31:46 -0400

    hello

--- src/index.js
+++ src/index.js
@@ -29,15 +29,7 @@ class App extends Component {
   }
 
   render() {
-    const videoSearch = _.debounce(term => {
-      this.videoSearch(term);
-    }, 300);
-
-    return (
-      <div>
-        hello
-      </div>
-    );
+    return <span>hello</div>;
   }
 }
 
