a0883dcfc5c4c58fe39eda3a9f2ebbba65e17e90
Deepak Acharya <deepak.acharya@DACHARYAMACOS1.attlocal.net>
2017-07-08 00:42:10 -0400

    hello

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,35 @@
 module.exports = {
-  entry: ["./src/index.js"],
+  entry: "./index.js",
   output: {
     path: __dirname,
     publicPath: "/",
     filename: "bundle.js"
   },
-  module: {
-    loaders: [
+  module:{
+    loaders:[
       {
-        exclude: /node_modules/,
-        loader: "babel"
+        test: /\.jsx?$/,
+        exclude: /node_modules|bower_components/,
+        loader:"babel"
+      },
+      {
+        test: /\.less$/,
+        loader: "style!css!less"
+      },
+      {
+        test:/\.css$/,
+        loader:"style!css"
+      },
+      {
+        test:/\.html$/,
+        loader: "file?name=[name].[ext]"
+      },
+      {
+        test: /\.(woff|eot|ttf|svg|gif|png|ico|woff(2))(\?.*)?$/,
+        loader: 'url-loader?limit=1000000'
+      },
+      {
+        include: /\.json/, loaders:["json-loader"]
       }
     ]
   },
