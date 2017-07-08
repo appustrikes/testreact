5dd4f5e49ad9d42c8cff18ac9383fc778fa9b51b
Deepak Acharya <deepak.acharya@DACHARYAMACOS1.attlocal.net>
2017-07-08 03:43:17 -0400

    hello

--- src/components/App.js
+++ src/components/App.js
@@ -4,10 +4,10 @@ export default class App extends React.Component {
   render () {
     return (
       <div>
-        <h1>Hello Shwetha</h1>
+        
+          <h1> VOTE FOR TREKHA</h1>
+<img src="../lo.jpg" height="500" width="500" / >
 
-        <h1>Hello Rekha</h1>
-          <div> VOTE FOR TREKHA</div>
       </div>
     )
   }
