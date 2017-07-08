90bd113e826f59b0db8805c2b7da76f9a71c1652
Deepak Acharya <deepak.acharya@DACHARYAMACOS1.attlocal.net>
2017-07-08 03:58:52 -0400

    hello

--- src/components/App.js
+++ src/components/App.js
@@ -7,6 +7,9 @@ export default class App extends React.Component {
         
           <h1> VOTE FOR TREKHA</h1>
 		<div> <h2> Make Herga Great Again </h2> </div>
+				<div> <h2>You will not find a better president in a whole milky way galaxy</h2> </div>
+				<div> <h2>Do not let the opportunity slip through your fingers , Please Vote me</h2> </div>
+
       </div>
     )
   }
