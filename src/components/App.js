e426057ec17373f3cebce75fddcfdd16d1b4621d
Deepak Acharya <deepak.acharya@DACHARYAMACOS1.attlocal.net>
2017-07-09 23:42:43 -0400

    hello

--- src/components/App.js
+++ src/components/App.js
@@ -2,17 +2,15 @@ import React from 'react'
 
 export default class App extends React.Component {
   render () {
+    var url = new URL(window.location.href 
+);
+var c = url.searchParams.get("a");
+
+console.log(c);
     return (
       <div>
         
-          <h1> VOTE FOR TREKHA</h1>
-		<div> <h2> MAKE HERGA GREAT AGAIN </h2> </div>
-				<div> <h2> "YOU WILL NOT FIND A BETTER PRESIDENT IN THE WHOLE MILKY WAY GALAXY"</h2> </div>
-                <div> <h2>"DO NOT LET THE OPPORTUNITY SLIP THROUGH YOUR FINGERS, PLEASE VOTE ME"</h2> </div>
-
-        <div><h1> "AB KI BAAR TREKHA SARKAAR"</h1></div>
-        <div> Please Vote and get free Bahubali tickets, including KFC coupons. Keep up to date on the latest sweepstakes and free gifts.</div> 
-      </div>
+Browser params {c}</div>
     )
   }
 }
